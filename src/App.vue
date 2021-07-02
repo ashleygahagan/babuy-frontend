06d51418b3e21ee162ddf86b77bb5e48f41ac08a
ashley <agahagan1@gmail.com>
2021-07-02 13:15:05 -0400

    makes it so login and signup go away once logged in

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/login">Login</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
+      <span v-if="isLoggedIn()">
+        <router-link to="/logout">Logout</router-link>
+      </span>
+      <span v-else>
+        <router-link to="/signup">Signup</router-link>
+        |
+        <router-link to="/login">Login</router-link>
+      </span>
     </div>
     <router-view />
   </div>
@@ -35,3 +38,16 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  methods: {
+    isLoggedIn: function () {
+      return localStorage.getItem("jwt");
+    },
+  },
+};
+</script>
